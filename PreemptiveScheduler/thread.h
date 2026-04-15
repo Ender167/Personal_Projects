c3264695dc94ce5a93ab74f31e48e87a16611825
Ender167 <cosminduka@yahoo.com>
2026-04-15 14:31:45 +0300

    Added documentation for Ecosystem Simulation. Fixed formating issues.

--- PreemptiveScheduler/thread.h
+++ PreemptiveScheduler/thread.h
@@ -9,16 +9,15 @@ typedef int thread_t;
 int thread_init(void);
 thread_t thread_self(void);
 void thread_yield(void);
-int thread_create(thread_t *tid, void (*fn) (void*), void *args);
+int thread_create(thread_t *tid, void (*fn)(void *), void *args);
 void thread_exit(void *ret_val);
 int thread_join(thread_t tid, void **ret_val);
 int setup_timer(void);
 
-
-typedef struct mutex{
-int is_locked;
-struct tcb *owner;
-struct tcb *waiting_queue;
+typedef struct mutex {
+  int is_locked;
+  struct tcb *owner;
+  struct tcb *waiting_queue;
 } mutex_t;
 
 int mutex_init(mutex_t *m);
@@ -26,11 +25,11 @@ int mutex_lock(mutex_t *m);
 int mutex_unlock(mutex_t *m);
 
 typedef struct rwlock {
-int readers;
-int writer;
-struct tcb *writer_owner;
-struct tcb *reader_queue;
-struct tcb *writer_queue;
+  int readers;
+  int writer;
+  struct tcb *writer_owner;
+  struct tcb *reader_queue;
+  struct tcb *writer_queue;
 } rwlock_t;
 
 int rwlock_init(rwlock_t *rw);
